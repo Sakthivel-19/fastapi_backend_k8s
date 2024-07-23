43c7a48e94fbdbd1cc607243c3afeab4d0d7800b
Sakthivel-19 <111050238+Sakthivel-19@users.noreply.github.com>
2024-07-23 18:26:59 +0530

    submodule changes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi_backend_k8s_sub_module.submodule.test import Test
 
 app = FastAPI()
 
@@ -8,5 +9,15 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/try")
-def read_root():
+def read_root_copy():
     return {"Hello": "Try"}
+
+@app.get("/test")
+def read_test():
+    res = Test.test_function()
+    return res
+
+@app.get("/testone")
+def read_test_copy():
+    res = Test.test_function_copy()
+    return res
